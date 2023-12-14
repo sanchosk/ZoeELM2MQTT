cf355b1eebe1a7102414bb0787e282172e71d8b6
Robert Klauco <github@klauco.ch>
2023-12-14 16:10:00 +0100

    Delete secrets.h

--- secrets.h
+++
@@ -1,7 +0,0 @@
-const char* ssid = "wlan_2_20";
-const char* password = "Bazenheid2015!20";
-const char* mqtt_server = "homeassistant.house";
-const char* mqtt_user = "mqttUser";
-const char* mqtt_pass = "mqttPass";
-const char* dongle_name = "Android-Vlink";
-const char* dongle_pin = "1234";
